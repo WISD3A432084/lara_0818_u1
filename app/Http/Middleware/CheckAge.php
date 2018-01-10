fd7f78e50053068ecd3919df4db529b8007231d9
Xin <lovemarks19@gmail.com>
2018-01-10 12:41:10 +0800

    撰寫 handle()

--- app/Http/Middleware/CheckAge.php
+++ app/Http/Middleware/CheckAge.php
@@ -15,6 +15,10 @@ class CheckAge
      */
     public function handle($request, Closure $next)
     {
+        if ($request->age <= 200) {
+            return redirect('home');
+        }
+
         return $next($request);
     }
 }
