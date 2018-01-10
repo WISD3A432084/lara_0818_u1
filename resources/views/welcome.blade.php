0e72d210c567d9364ee355861a03f1c530dc2db0
Xin <lovemarks19@gmail.com>
2018-01-10 12:23:35 +0800

    在 welcome.blade.php 視圖產生一個 qrcode

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,13 +81,16 @@
                 <div class="title m-b-md">
                     Laravel
                 </div>
-
                 <div class="links">
                     <a href="https://laravel.com/docs">Documentation</a>
                     <a href="https://laracasts.com">Laracasts</a>
                     <a href="https://laravel-news.com">News</a>
                     <a href="https://forge.laravel.com">Forge</a>
                     <a href="https://github.com/laravel/laravel">GitHub</a>
+                </div>
+                <div class="visible-print text-center">
+                    {!! QrCode::size(100)->generate(Request::url()); !!}
+                    <p>Scan me to return to the original page.</p>
                 </div>
             </div>
         </div>
