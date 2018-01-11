fc537fbe90f553712bfebe90c3511a51895fd867
Xin <lovemarks19@gmail.com>
2018-01-11 14:35:00 +0800

    artisan make:auth (身分驗證)

--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,7 @@ Route::get('/', function () {
 Route::get('/tracy', function () {
     throw new \Exception('Tracyworks!');
 });
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
